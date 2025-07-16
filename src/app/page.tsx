e65a93458f4735a3055c9afdca9a2489d66384d2
David D <david.m.dee01@gmail.com>
2025-07-16 18:07:04 +0000

    Changed front page image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ type AllPostsData = {
 }[]
 
 // Change this url to change the image!
-const imageUrl = '/images/mountain.jpg';
+const imageUrl = '/images/at.webp';
 
 export default function Home() {
   const allPostsData: AllPostsData = getSortedPostsData()
