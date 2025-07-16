b40b9763920bf121926f001ff4a7b72ced136c36
David D <david.m.dee01@gmail.com>
2025-07-16 17:50:45 +0000

    Changed name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ TODO:  Change these things along with:
   - favicon.ico in /public
  */
 const font = Poppins({ weight: '700', subsets: ['latin']})
-const title = 'Somebody\'s Site';
+const title = 'David\'s Site';
 const description = 'This my personal site';
 const links = [
   {title: 'Pokemon', href: '/pokemon'},
